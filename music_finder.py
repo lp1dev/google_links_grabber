0d3dd6c1dc58b46961b757bc399be32680b59508
Lupin <mail@lupin.me>
2016-02-04 12:35:48 +0100

    finished finder

--- music_finder.py
+++ music_finder.py
@@ -18,26 +18,53 @@ def usage():
     print("usage : %s query" %argv[0])
     return 1
 
-def fetch_file(link):
+def is_indexof(text):
+    for identifier in indexof_identifiers:
+        if identifier not in text:
+            print("[Identifier %s not found]" %(identifier))
+            return False
+    return True
+
+def fetch_file_links(link):
     r = requests.get("http://"+link)
     if verbose:
         print("fetching files on %s" %link)
-    for identifier in indexof_identifiers:
-        if identifier not in r.text:
-            if verbose:
-                print("[%s not in %s]" %(identifier, link))
-            return False
-    print("[%s passed test 1]" %link)
-    print(get_links_from_text(r.text))
+    if not is_indexof(r.text):
+        if verbose:
+            print("[%s not an open dirlisting]" %link)
+        return []
+    links = []
+    splits = r.text.split('a href="')
+    for split in splits:
+        find = split.find('"')
+        if split.find('<') < find:
+            find = split.find('<')
+        if len(split[:find]) > 0:
+            links.append(split[:find].replace("&amp;prev=search", ""))
+    return links
+
+def get_matching_links(links, query):
+    matching_links = []
+    query_terms = query.split(" ")
+    for link in links:
+        if link[-4:] == ".mp3":
+            match = 0
+            for term in query_terms:
+                if term in link:
+                    match += 1
+            if match >= 1:
+                links.append(matching_links)
+    return matching_links
 
 def main():
     if len(argv) != 2:
         return usage()
     links = get_links(query_string %argv[1], 0)
     for link in links:
-        lol
-        fetch_file(link)
-        sleep(sleep_time)            
+        file_links = fetch_file_links(link)
+        matching_links = get_matching_links(file_links, argv[1])
+        print(matching_links)
+        sleep(sleep_time)
     return 0
 
 if __name__ == "__main__":
