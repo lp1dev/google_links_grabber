1f8c3e29d1bd4bab8553b92161bd22827ebf533a
Lupin <mail@lupin.me>
2016-02-04 18:39:35 +0100

    working POC with searx

--- music_finder.py
+++ music_finder.py
@@ -8,9 +8,9 @@ from sys import argv
 from time import sleep
 import requests
 
-max_threads=4
+max_threads=8
 sleep_time = 0
-query_string = 'intitle%%3A%%22index%%20of%%22%%20%%2B%%22last%%20modified%%22%%20%%2B%%22parent%%20directory%%22%%20%%2B(wma%%7Cmp3%%7Cogg)%%20%s'
+query_string = 'index of last modified parent directory wma mp3 ogg %s'
 indexof_identifiers=["Index of", "Name", "Last modified", "Size", "Description", "Parent Directory"]
 verbose=True
 
@@ -21,7 +21,8 @@ def usage():
 def is_indexof(text):
     for identifier in indexof_identifiers:
         if identifier not in text:
-            print("[Identifier %s not found]" %(identifier))
+            if verbose:
+                print("\t[Identifier %s not found]" %(identifier))
             return False
     return True
 
@@ -31,7 +32,7 @@ def fetch_file_links(link):
         print("fetching files on %s" %link)
     if not is_indexof(r.text):
         if verbose:
-            print("[%s not an open dirlisting]" %link)
+            print("\t[%s not an open dirlisting]" %link)
         return []
     links = []
     splits = r.text.split('a href="')
@@ -43,28 +44,38 @@ def fetch_file_links(link):
             links.append(split[:find].replace("&amp;prev=search", ""))
     return links
 
-def get_matching_links(links, query):
+def get_matching_links(origin_url, links, query):
     matching_links = []
     query_terms = query.split(" ")
     for link in links:
-        if link[-4:] == ".mp3":
+        if type(link) == str and link[-4:].lower() == ".mp3":
             match = 0
             for term in query_terms:
-                if term in link:
+                if verbose:
+                    print("\t\t[Checking if %s in %s]" %(term.lower(), link.lower()))
+                if term.replace(" ", "").lower() in link.lower():
+                    if verbose:
+                        print("\t\t[\033[0;32m%s FOUND in %s match +1\033[0;0m]" %(term, link)) 
                     match += 1
-            if match >= 1:
-                links.append(matching_links)
+            if match == len(query_terms):
+                matching_links.append(origin_url+link)
     return matching_links
+
+def handle_link(link):
+    file_links = fetch_file_links(link)
+    matching_links = get_matching_links(link, file_links, argv[1])
+    with open("output.txt", "a+") as f:
+        for link in matching_links:
+            f.write(link+"\n")
 
 def main():
     if len(argv) != 2:
         return usage()
     links = get_links(query_string %argv[1], 0)
     for link in links:
-        file_links = fetch_file_links(link)
-        matching_links = get_matching_links(file_links, argv[1])
-        print(matching_links)
-        sleep(sleep_time)
+        with ProcessPoolExecutor(max_workers=max_threads) as e:
+            thread = [e.submit(handle_link, link)]
+            sleep(sleep_time)
     return 0
 
 if __name__ == "__main__":
