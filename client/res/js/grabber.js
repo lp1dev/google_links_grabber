e1e97cc8be9b0c8c02daebda9554623c0a3faf9d
Jeremie Amsellem <lupin@lp1.eu>
2016-02-05 15:01:01 +0100

    added predictive search?

--- client/res/js/grabber.js
+++ client/res/js/grabber.js
@@ -1,29 +1,41 @@
 var lastData = {};
 var isFetching = false;
 var interval;
+var search_url = "http://2.lp1.eu:5000/search/"
+var fetch_url = "http://2.lp1.eu:5000/fetch/"
+
+$(document).ready(function(){
+    $("#search").on('input', function(){
+	var query = $("#search").val()
+
+	console.log("input has changed");
+	$("#contentDiv").empty();
+	if (query.length > 1)
+	    $.getJSON(search_url+$("#search").val().toLowerCase(), predictive_input);
+    });
+});
+
+function predictive_input(data){
+    $.each(data, function(key) {
+	var htmlContent = $("#contentDiv").html()+"<p style='color:red' onclick='onLinkClick(this);'>"+key+"</p>";
+	$("#contentDiv").html(htmlContent);
+    });
+}
 
 function hasChanged(){
-    var query = $("#search").val();
-    $.getJSON('http://2.lp1.eu:5000/search/'+query, success);
-    
-    function success(data) {
+    var query = $("#search").val().toLowerCase();
+    $.getJSON(search_url+query, search_success);
+
+    function search_success(data) {
 	$("#contentDiv").empty();
-	if (!isFetching)
-	    $("#contentDiv").html("<p>Do you mean :</p>");
-	else
-	    $("#contentDiv").html("<p>Nothing found, fetching...</p>");
+	if (isFetching)
+	    $("#contentDiv").html("<p>Nothing in database, seeking external sources...</p>");
 	lastData = data;
 	console.log(data);
-	console.log(data.length);
-	$.each(data, function(key) {
-	    var htmlContent = $("#contentDiv").html()+"<p style='color:red' onclick='onLinkClick(this);'>"+key+"</p>";
-	    $("#contentDiv").html(htmlContent);
-	});
 	if ($.isEmptyObject(data)){
 	    if (isFetching == false){
 		isFetching = true;
-		console.log("data is null, fetching");
-		$.getJSON('http://2.lp1.eu:5000/fetch/'+query, fetch_success);
+		$.getJSON(fetch_url+query, fetch_success);
 		$("#contentDiv").html("Loading...");
 		interval = setInterval(function() {
 		    if (isFetching)
@@ -31,9 +43,15 @@ function hasChanged(){
 		}, 8000);
 	    }
 	}
+	else{
+	    if (isFetching == true)
+		isFetching = false;
+	    clearInterval(interval);
+	}
     }
     
     function fetch_success(data) {
+	isFetching = false;
 	clearInterval(interval);
     }
 }
