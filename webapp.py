773a1c7a1083e60b116d7540b9dd1f659d7730ae
lupin <mail@lp1.eu>
2016-02-05 14:32:56 +0100

    added headers for CORS

--- webapp.py
+++ webapp.py
@@ -21,5 +21,12 @@ def fetch(query):
     success, value = fetch_links(query)
     return value
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept")
+    response.headers.add('Content-Type','application/json')
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
